2b53196b1d3e2e86bf04c5595a0155a5c81a689b
skgydnjs@gmail.com <skgydnjs@gmail.com>
2023-04-04 00:26:31 +0900

    lambda

--- src/main/java/jungsuk/lambda/LambdaEx_1.java
+++ src/main/java/jungsuk/lambda/LambdaEx_1.java
@@ -16,6 +16,9 @@ public class LambdaEx_1 {
      * 2. 반환값이 있는 경우 식이나 값만 적고 return 문 생략 가능
      * 3. 매개변수의 타입이 추론 가능하면 생략가능
      *
+     * 함수와 메서드의 차이
+     * - 근본적으로 동일. 함수는 일반적 용어, 메서드는 객체지향개념 용어
+     * - 함수는 클래스에 독립적, 메서드는 클래스에 종속적 -> 자바에서는 메서드만 있음
      * 작성 시 주의사항
      * 1. 매개변수가 하나인 경우, 괄호() 생략가능(타입이 없을 때만)
      * 2. 하나뿐인 문장이 return문이면 괄호{} 생략 불가
@@ -27,19 +30,52 @@ public class LambdaEx_1 {
 
     public static void main(String[] args) {
 
-//        Object obj =  (a, b) -> a > b ? a : b // 람다식. 에러
 
-        Object obj = new Object() {
-                int max(int a, int b) {
-                    return a > b ?  a : b;
-                }
-            };
+        MyFunction2 f = (a,b) -> a > b ? a : b;
 
-//        int value = obj.max(3, 5); // 에러, Object타입에는 max라는 메서드가 없기 때문에 호출할 수 없다.
-        // -> 함수형 인터페이스로 해결..
+        int value = f.max(3,5); // 함수형 인터페이스
+        System.out.println("value = " + value);
 
+        MyFunction f1 = () -> System.out.println("f1.run()");
 
+        MyFunction f2 = new MyFunction() {
+            @Override
+            public void run() {
+                System.out.println("f2.run()");
+            }
+        };
+
+        MyFunction f3 = getMyFunction();
+
+        f1.run();
+        f2.run();
+        f3.run();
+
+        execute(f1);
+        execute( () -> System.out.println("run()"));
 
     }
 
+    static void execute(MyFunction f) { // 매개변수 타입이 Myfunction인 메서드
+        f.run();
+    }
+
+    static MyFunction getMyFunction() { // 반환 타입이 MyFunction인 메서드
+        MyFunction f  = () -> System.out.println("f3.run()");
+        return f;
+    }
+
+
+
+}
+
+@FunctionalInterface
+interface  MyFunction {
+    void run();
+}
+
+@FunctionalInterface // 추상 메서드가 하나만 가져야 한다.
+interface MyFunction2 {
+    //public abstract int max(int a, int b);
+    int max(int a, int b);
 }
